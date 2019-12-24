18cb8bbaf2b814099e8b33cd81e02dc625a0e0c9
Coding Cat <32220838+qtulhu@users.noreply.github.com>
2019-12-24 14:15:24 +0800

    id

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 TOKEN = '920710380:AAG8uT7mRjpMXDkY13v4OZyrxt2jMV0JE6Y'
-CHAT = 'None'
+CHAT = '-345520404'
 start_mess = 'Привет! \n\nПиши сюда свои вопросы, и мы ответим на них!'
 help_mess = 'Это бот помошник'
 success_mess = 'Мы получили твоё сообщение!'
