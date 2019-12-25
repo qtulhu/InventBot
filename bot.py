100589d91df22f920849a183b4276a9268b1f367
Coding Cat <32220838+qtulhu@users.noreply.github.com>
2019-12-25 10:03:17 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,29 @@
+# -*- coding: utf-8 -*-
 import telebot
-from variables import *
-import logging
-
-bot = telebot.TeleBot(TOKEN)
-
-def main(use_logging, level_name):
-	if use_logging:
-		telebot.logger.setLevel(logging.getLevelName(level_name))
-	bot.polling(none_stop=True, interval=.5)
-
-@bot.message_handler(commands=['help'])
-def help_handler(message):
-	bot.send_message(message.from_user.id, help_mess)
-
-@bot.message_handler(func=lambda message: True)
-def forward_handler(message):
-	try:
-		if message.chat.id == int(CHAT):
-			bot.send_message(message.reply_to_message.forward_from.id, message.text)
-		else:
-			bot.forward_message(CHAT,message.chat.id)
-	except Exception as error:
-		print("Info:{}".format(error))
 
 
-if __name__ == '__main__':
-	main(True, 'DEBUG')
+bot = telebot.TeleBot("920710380:AAG8uT7mRjpMXDkY13v4OZyrxt2jMV0JE6Y")
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    if message.text == "Hi":
+        bot.send_message(message.from_user.id, "Hello! I am HabrahabrExampleBot. How can i help you?")
+    
+    elif message.text == "How are you?" or message.text == "How are u?":
+        bot.send_message(message.from_user.id, "I'm fine, thanks. And you?")
+    
+    else:
+        bot.send_message(message.from_user.id, "Sorry, i dont understand you.")
+
+bot.polling(none_stop=True, interval=0)
+
+# Обработчик команд '/start' и '/help'.
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+    pass
+
+ # Обработчик для документов и аудиофайлов
+@bot.message_handler(content_types=['document', 'audio'])
+def handle_document_audio(message):
+    pass
+
+bot.polling(none_stop=True, interval=0)
