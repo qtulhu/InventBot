ae2dd3c63278eee97d88b6244122b6710d4b5b1b
Coding Cat <32220838+qtulhu@users.noreply.github.com>
2019-12-25 10:48:42 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,22 @@ import telebot
 
 
 bot = telebot.TeleBot("920710380:AAG8uT7mRjpMXDkY13v4OZyrxt2jMV0JE6Y")
-
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    if message.text == "Hi":
+        bot.send_message(message.from_user.id, "Hello! I am HabrahabrExampleBot. How can i help you?")
+    
+    elif message.text == "How are you?" or message.text == "How are u?":
+        bot.send_message(message.from_user.id, "I'm fine, thanks. And you?")
+    
+    else:
+        bot.send_message(message.from_user.id, "Sorry, i dont understand you.")
 
 
 
 # Обработчик команд '/start' и '/help'.
 @bot.message_handler(commands=['start'])
-def start(message):
+def handle_start(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*[types.KeyboardButton(name) for name in ['Моё местоположение...', 'Инвентаризация', 'Инструкции']])
     msg = bot.send_message(message.chat.id, 'Давай сначала определим где ты находишься, а затем произведем инвентаризацию',
@@ -25,4 +34,4 @@ def start(message):
 def handle_document_audio(message):
     pass
 
-bot.polling(none_stop=True, interval=0)
+bot.polling()
