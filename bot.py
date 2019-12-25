4b08bd2c7ccd7962ccf21134001787ab2f32e109
Coding Cat <32220838+qtulhu@users.noreply.github.com>
2019-12-25 12:16:14 +0800

    hook

--- bot.py
+++ bot.py
@@ -1,45 +1,62 @@
 # -*- coding: utf-8 -*-
 import os
 import telebot
-from flask import Flask, request
+
+from queue import Queue  # in python 2 it should be "from Queue"
+from threading import Thread
+
+from telegram import Bot
+from telegram.ext import Dispatcher
 
 
-TOKEN = "920710380:AAG8uT7mRjpMXDkY13v4OZyrxt2jMV0JE6Y"
-PORT = int(os.environ.get('PORT','8443'))
-updater = Updater(TOKEN)
-bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=["start"])
-def start(m):
-    msg = bot.send_message(m.chat.id, "Вас приветствует Бот")
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(*[types.KeyboardButton(name) for name in ['О компании', 'Прайс-лист']])
-    keyboard.add(*[types.KeyboardButton(name) for name in ['Акции', 'Контакты']])
-    bot.send_message(m.chat.id, 'Выберите в меню что вам интересно!',
-        reply_markup=keyboard)
-    bot.register_next_step_handler(msg, name)
 
-def name(m):
-    if m.text == 'О компании':
+token = "920710380:AAG8uT7mRjpMXDkY13v4OZyrxt2jMV0JE6Y"
+bot = telebot.TeleBot(token)
+
+def setup(token):
+    # Create bot, update queue and dispatcher instances
+    bot = Bot(token)
+    update_queue = Queue()
+    
+    dispatcher = Dispatcher(bot, update_queue)
+
+
+    @bot.message_handler(commands=["start"])
+    def start(m):
+        msg = bot.send_message(m.chat.id, "Вас приветствует Бот")
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add(*[types.KeyboardButton(advert) for advert in ['Сертификаты']])
-        keyboard.add(*[types.KeyboardButton(advert) for advert in ['В начало']])
-        bot.send_message(m.chat.id, 'инфа о компании',
+        keyboard.add(*[types.KeyboardButton(name) for name in ['О компании', 'Прайс-лист']])
+        keyboard.add(*[types.KeyboardButton(name) for name in ['Акции', 'Контакты']])
+        bot.send_message(m.chat.id, 'Выберите в меню что вам интересно!',
             reply_markup=keyboard)
-    elif m.text == 'Прайс-лист':
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add(*[types.KeyboardButton(advert) for advert in ['Общий', 'Одиночный']])
-        keyboard.add(*[types.KeyboardButton(advert) for advert in ['В начало']])
-        bot.send_message(m.chat.id, 'Выберите прайс который нужен.',
-            reply_markup=keyboard)
-    elif m.text == 'Акции':
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add(*[types.KeyboardButton(advert) for advert in ['В начало']])
-        bot.send_message(m.chat.id, 'Сожалею, но в данный момент акций нет(',
-            reply_markup=keyboard)
+        bot.register_next_step_handler(msg, name)
+
+    def name(m):
+        if m.text == 'О компании':
+            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            keyboard.add(*[types.KeyboardButton(advert) for advert in ['Сертификаты']])
+            keyboard.add(*[types.KeyboardButton(advert) for advert in ['В начало']])
+            bot.send_message(m.chat.id, 'инфа о компании',
+                reply_markup=keyboard)
+        elif m.text == 'Прайс-лист':
+            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            keyboard.add(*[types.KeyboardButton(advert) for advert in ['Общий', 'Одиночный']])
+            keyboard.add(*[types.KeyboardButton(advert) for advert in ['В начало']])
+            bot.send_message(m.chat.id, 'Выберите прайс который нужен.',
+                reply_markup=keyboard)
+        elif m.text == 'Акции':
+            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            keyboard.add(*[types.KeyboardButton(advert) for advert in ['В начало']])
+            bot.send_message(m.chat.id, 'Сожалею, но в данный момент акций нет(',
+                reply_markup=keyboard)
 
 
 
-updater.start_webhook(listen="127.0.0.1", port=PORT, url_path=TOKEN)
-updater.bot.set_webhook("https://iventbot.herokuapp.com" + TOKEN)
-updater.idle()
+
+
+# Start the thread
+    thread = Thread(target=dispatcher.start, name='dispatcher')
+    thread.start()
+    
+    return update_queue
